cdc596650d538c7a6b3dc2493bf194cadece95a1
atahalicioglu <ahalicioglu20@ku.edu.tr>
2024-04-17 23:01:16 +0300

    first commit

---
+++ example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+	
+	printf("Number of arguments: %d.\n", argc);
+	printf("First argument: %s.\n", argv[0]);
+	printf("Hello\n");
+
+	return 0;
+}
