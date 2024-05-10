dacb20d2b2eb06a53ae76e755f9e425095b3f3a3
ahalicioglu20 <ahalicioglu20@ku.edu.tr>
2024-05-10 19:56:24 +0300

    changed name and location

---
+++ src/hshell.c
@@ -0,0 +1,595 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <termios.h> // termios, TCSANOW, ECHO, ICANON
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+const char *sysname = "mishell";
+
+enum return_codes {
+	SUCCESS = 0,
+	EXIT = 1,
+	UNKNOWN = 2,
+};
+
+struct command_t {
+	char *name;
+	bool background;
+	bool auto_complete;
+	int arg_count;
+	char **args;
+	char *redirects[3]; // in/out redirection
+	struct command_t *next; // for piping
+};
+
+/**
+ * Prints a command struct
+ * @param struct command_t *
+ */
+void print_command(struct command_t *command) {
+	int i = 0;
+	printf("Command: <%s>\n", command->name);
+	printf("\tIs Background: %s\n", command->background ? "yes" : "no");
+	printf("\tNeeds Auto-complete: %s\n",
+		   command->auto_complete ? "yes" : "no");
+	printf("\tRedirects:\n");
+
+	for (i = 0; i < 3; i++) {
+		printf("\t\t%d: %s\n", i,
+			   command->redirects[i] ? command->redirects[i] : "N/A");
+	}
+
+	printf("\tArguments (%d):\n", command->arg_count);
+
+	for (i = 0; i < command->arg_count; ++i) {
+		printf("\t\tArg %d: %s\n", i, command->args[i]);
+	}
+
+	if (command->next) {
+		printf("\tPiped to:\n");
+		print_command(command->next);
+	}
+}
+
+/**
+ * Release allocated memory of a command
+ * @param  command [description]
+ * @return         [description]
+ */
+int free_command(struct command_t *command) {
+	if (command->arg_count) {
+		for (int i = 0; i < command->arg_count; ++i)
+			free(command->args[i]);
+		free(command->args);
+	}
+
+	for (int i = 0; i < 3; ++i) {
+		if (command->redirects[i])
+			free(command->redirects[i]);
+	}
+
+	if (command->next) {
+		free_command(command->next);
+		command->next = NULL;
+	}
+
+	free(command->name);
+	free(command);
+	return 0;
+}
+
+/**
+ * Show the command prompt
+ * @return [description]
+ */
+int show_prompt() {
+	char cwd[1024], hostname[1024];
+	gethostname(hostname, sizeof(hostname));
+	getcwd(cwd, sizeof(cwd));
+	// Changing the color so that it will look more like the regular shell
+	
+	// I found the resources for changing the color in the internet
+	// www.theurbanpenguin.com/4184-2/
+
+	// Green for the user and hostname
+	printf("\033[1;32m");
+	printf("%s@%s:",getenv("USER"), hostname);
+	// Blue for the cwd
+	printf("\033[1;34m");
+	printf("%s",cwd);
+	// Red  for the sysname (My choice)
+	printf("\033[0;31m");
+	printf(" %s$ ",sysname);
+	// Turning back to default color
+	printf("\033[0m");
+	//printf("%s@%s:%s %s$ ", getenv("USER"), hostname, cwd, sysname);
+	return 0;
+}
+
+/**
+ * Parse a command string into a command struct
+ * @param  buf     [description]
+ * @param  command [description]
+ * @return         0
+ */
+int parse_command(char *buf, struct command_t *command) {
+	const char *splitters = " \t"; // split at whitespace
+	int index, len;
+	len = strlen(buf);
+
+	// trim left whitespace
+	while (len > 0 && strchr(splitters, buf[0]) != NULL) {
+		buf++;
+		len--;
+	}
+
+	while (len > 0 && strchr(splitters, buf[len - 1]) != NULL) {
+		// trim right whitespace
+		buf[--len] = 0;
+	}
+
+	// auto-complete
+	if (len > 0 && buf[len - 1] == '?') {
+		command->auto_complete = true;
+	}
+
+	// background
+	if (len > 0 && buf[len - 1] == '&') {
+		command->background = true;
+	}
+
+	char *pch = strtok(buf, splitters);
+	if (pch == NULL) {
+		command->name = (char *)malloc(1);
+		command->name[0] = 0;
+	} else {
+		command->name = (char *)malloc(strlen(pch) + 1);
+		strcpy(command->name, pch);
+	}
+
+	command->args = (char **)malloc(sizeof(char *));
+
+	int redirect_index;
+	int arg_index = 0;
+	char temp_buf[1024], *arg;
+
+	while (1) {
+		// tokenize input on splitters
+		pch = strtok(NULL, splitters);
+		if (!pch)
+			break;
+		arg = temp_buf;
+		strcpy(arg, pch);
+		len = strlen(arg);
+
+		// empty arg, go for next
+		if (len == 0) {
+			continue;
+		}
+
+		// trim left whitespace
+		while (len > 0 && strchr(splitters, arg[0]) != NULL) {
+			arg++;
+			len--;
+		}
+
+		// trim right whitespace
+		while (len > 0 && strchr(splitters, arg[len - 1]) != NULL) {
+			arg[--len] = 0;
+		}
+
+		// empty arg, go for next
+		if (len == 0) {
+			continue;
+		}
+
+		// piping to another command
+		if (strcmp(arg, "|") == 0) {
+			struct command_t *c = malloc(sizeof(struct command_t));
+			int l = strlen(pch);
+			pch[l] = splitters[0]; // restore strtok termination
+			index = 1;
+			while (pch[index] == ' ' || pch[index] == '\t')
+				index++; // skip whitespaces
+
+			parse_command(pch + index, c);
+			pch[l] = 0; // put back strtok termination
+			command->next = c;
+			continue;
+		}
+
+		// background process
+		if (strcmp(arg, "&") == 0) {
+			// handled before
+			continue;
+		}
+
+		// handle input redirection
+		redirect_index = -1;
+		if (arg[0] == '<') {
+			redirect_index = 0;
+		}
+
+		if (arg[0] == '>') {
+			if (len > 1 && arg[1] == '>') {
+				redirect_index = 2;
+				arg++;
+				len--;
+			} else {
+				redirect_index = 1;
+			}
+		}
+
+		if (redirect_index != -1) {
+			command->redirects[redirect_index] = malloc(len);
+			strcpy(command->redirects[redirect_index], arg + 1);
+			continue;
+		}
+
+		// normal arguments
+		if (len > 2 &&
+			((arg[0] == '"' && arg[len - 1] == '"') ||
+			 (arg[0] == '\'' && arg[len - 1] == '\''))) // quote wrapped arg
+		{
+			arg[--len] = 0;
+			arg++;
+		}
+
+		command->args =
+			(char **)realloc(command->args, sizeof(char *) * (arg_index + 1));
+
+		command->args[arg_index] = (char *)malloc(len + 1);
+		strcpy(command->args[arg_index++], arg);
+	}
+	command->arg_count = arg_index;
+
+	// increase args size by 2
+	command->args = (char **)realloc(
+		command->args, sizeof(char *) * (command->arg_count += 2));
+
+	// shift everything forward by 1
+	for (int i = command->arg_count - 2; i > 0; --i) {
+		command->args[i] = command->args[i - 1];
+	}
+
+	// set args[0] as a copy of name
+	command->args[0] = strdup(command->name);
+
+	// set args[arg_count-1] (last) to NULL
+	command->args[command->arg_count - 1] = NULL;
+
+	return 0;
+}
+
+void prompt_backspace() {
+	putchar(8); // go back 1
+	putchar(' '); // write empty over
+	putchar(8); // go back 1 again
+}
+
+int is_executable(const char *path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) != 0) return 0; // Failed to get info, assume not executable
+    return (statbuf.st_mode & S_IXUSR) || (statbuf.st_mode & S_IXGRP) || (statbuf.st_mode & S_IXOTH);
+}
+
+/**
+ * Prompt a command from the user
+ * @param  buf      [description]
+ * @param  buf_size [description]
+ * @return          [description]
+ */
+int prompt(struct command_t *command) {
+	size_t index = 0;
+	char c;
+	char buf[4096];
+	static char oldbuf[4096];
+
+	// tcgetattr gets the parameters of the current terminal
+	// STDIN_FILENO will tell tcgetattr that it should write the settings
+	// of stdin to oldt
+	static struct termios backup_termios, new_termios;
+	tcgetattr(STDIN_FILENO, &backup_termios);
+	new_termios = backup_termios;
+	// ICANON normally takes care that one line at a time will be processed
+	// that means it will return if it sees a "\n" or an EOF or an EOL
+	new_termios.c_lflag &=
+		~(ICANON |
+		  ECHO); // Also disable automatic echo. We manually echo each char.
+	// Those new settings will be set to STDIN
+	// TCSANOW tells tcsetattr to change attributes immediately.
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+	// printf("\nOld Buffer %s\n", buf);
+	buf[0] = 0;
+	memset(buf, 0, sizeof(char)*(int) strlen(buf));
+	// printf("New Buffer: %s\n", buf);
+	show_prompt();
+	while (1) {
+		c = getchar();
+		// printf("Keycode: %u\n", c); // DEBUG: uncomment for debugging
+
+		// handle tab
+		if (c == 9) {
+			
+			char cwd[1024];
+			if (getcwd(cwd, sizeof(cwd)) == NULL) {
+				perror("getcwd");
+				return 1;
+			}
+			
+			char *path_env = getenv("PATH");
+			if (!path_env) {
+				path_env = "";
+			}
+
+			char *new_path = malloc(strlen(cwd) + strlen(path_env) + 2);
+			sprintf(new_path, "%s:%s", path_env, cwd);
+
+			char **matches = NULL;
+    		int match_count = 0;
+			int buf_len = strlen(buf);
+			
+			char *dir = strtok(new_path, ":");
+			while (dir != NULL) {
+				struct dirent *entry;
+				DIR *dp = opendir(dir);
+				if (dp != NULL) {
+					while ((entry = readdir(dp)) != NULL) {
+						char full_path[1024];
+						sprintf(full_path, "%s/%s", dir, entry->d_name);
+
+						if (entry->d_type == DT_REG && is_executable(full_path)) {
+							// printf("%s\n", entry->d_name);
+							// In here, compare it with the ongiong command
+							// printf("%s\n",buf);
+							if (strncmp(buf, entry->d_name, buf_len) == 0) {
+								// printf("%s\n",entry->d_name);
+								matches = realloc(matches, (match_count + 1) * sizeof(char *));
+								matches[match_count++] = strdup(entry->d_name);
+							}
+						}
+					}
+					closedir(dp);
+				} else {
+					perror("opendir");
+				}
+				dir = strtok(NULL, ":");
+			}
+			printf("\nMatch count: %d", match_count);
+			free(new_path);
+
+			if (match_count > 0) {
+				for (int i = 0; i < match_count; i++) {
+					printf("\n%s", matches[i]);
+					free(matches[i]);
+				}
+				free(matches);
+			}
+			//printf("\nBuffer size: %zu\n", strlen(buf));
+			show_prompt();
+			printf("%s", buf);
+			//buf[index--] = 0;
+			/*
+			buf[index++] = '?'; // autocomplete
+			break;
+			*/
+		}	
+
+		// handle backspace
+		if (c == 127) {
+			if (index > 0) {
+				prompt_backspace();
+				index--;
+			}
+			continue;
+		}
+
+		if (c == 27 || c == 91 || c == 66 || c == 67 || c == 68) {
+			continue;
+		}
+
+		// up arrow
+		if (c == 65) {
+			while (index > 0) {
+				prompt_backspace();
+				index--;
+			}
+
+			char tmpbuf[4096];
+			printf("%s", oldbuf);
+			strcpy(tmpbuf, buf);
+			strcpy(buf, oldbuf);
+			strcpy(oldbuf, tmpbuf);
+			index += strlen(buf);
+			continue;
+		}
+
+		if (c != 9) {
+			putchar(c); // echo the character
+			buf[index++] = c;
+		}
+
+		if (index >= sizeof(buf) - 1)
+			break;
+		if (c == '\n') // enter key
+			break;
+		if (c == 4) // Ctrl+D
+			return EXIT;
+	}
+
+	//printf("Exited");
+	// trim newline from the end
+	if (index > 0 && buf[index - 1] == '\n') {
+		index--;
+	}
+
+	// null terminate string
+	buf[index++] = '\0';
+
+	strcpy(oldbuf, buf);
+
+	parse_command(buf, command);
+	memset(buf, 0, sizeof(char)*(int) strlen(buf));
+	print_command(command); // DEBUG: uncomment for debugging
+
+	// restore the old settings
+	tcsetattr(STDIN_FILENO, TCSANOW, &backup_termios);
+	return SUCCESS;
+}
+
+int process_command(struct command_t *command);
+
+int main() {
+	while (1) {
+		struct command_t *command = malloc(sizeof(struct command_t));
+
+		// set all bytes to 0
+		memset(command, 0, sizeof(struct command_t));
+
+		int code;
+		code = prompt(command);
+		if (code == EXIT) {
+			break;
+		}
+
+		code = process_command(command);
+		if (code == EXIT) {
+			break;
+		}
+
+		free_command(command);
+	}
+
+	printf("\n");
+	return 0;
+}
+
+char *get_path(char *command_name) {
+
+	char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd");
+        return NULL;
+    }
+
+    // Get the PATH environment variable
+    char *path_env = getenv("PATH");
+    if (!path_env) {
+        path_env = "";
+    }
+
+	// Concatinating cwd with env PATH
+    char *new_path = malloc(strlen(cwd) + strlen(path_env) + 2);
+    sprintf(new_path, "%s:%s", path_env, cwd);
+
+    // Check each directory in the concateneted path
+    char *resolved_path = NULL;
+    char *dir = strtok(new_path, ":");
+    while (dir != NULL) {
+        char *full_path = malloc(strlen(dir) + strlen(command_name) + 2);
+        sprintf(full_path, "%s/%s", dir, command_name);
+        if (access(full_path, X_OK) == 0) {
+            resolved_path = strdup(full_path);
+            free(full_path);
+            break;
+        }
+        free(full_path);
+        dir = strtok(NULL, ":");
+    }
+
+    free(new_path);
+    return resolved_path;
+}
+
+int process_command(struct command_t *command) {
+	int r;
+
+	if (strcmp(command->name, "") == 0) {
+		return SUCCESS;
+	}
+
+	if (strcmp(command->name, "exit") == 0) {
+		return EXIT;
+	}
+
+	if (strcmp(command->name, "cd") == 0) {
+		if (command->arg_count > 0) {
+			r = chdir(command->args[1]);
+			if (r == -1) {
+				printf("-%s: %s: %s\n", sysname, command->name,
+					   strerror(errno));
+			}
+
+			return SUCCESS;
+		}
+	}
+
+	int num_pipes = 0;
+    for (struct command_t *cmd = command; cmd->next != NULL; cmd = cmd->next) {
+        num_pipes++;
+    }
+
+	printf("Number of pipes: %d\n", num_pipes);
+
+    int pipes[num_pipes][2];
+    for (int i = 0; i < num_pipes; i++) {
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            return EXIT_FAILURE;
+        }
+    }
+	int i = 0;
+    pid_t pid;
+    while (i <= num_pipes) {
+        pid = fork();
+        if (pid == 0) { 
+            
+            if (i > 0) {
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+            }
+            if (command->next != NULL) {
+                dup2(pipes[i][1], STDOUT_FILENO);
+            }
+            
+            for (int j = 0; j < num_pipes; j++) {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+			
+            char *command_path = get_path(command->name);
+            if (!command_path) {
+                printf("Command not found.\n");
+                return UNKNOWN;
+            }
+
+            execv(command_path, command->args);
+            perror("execv");
+            return EXIT_FAILURE;
+        } else if (pid < 0) {
+            perror("fork");
+            return EXIT_FAILURE;
+		}
+		if (command->next != NULL){
+        	command = command->next;
+		}
+		i++;
+	}
+ 
+    for (int j = 0; j < num_pipes; j++) {
+        close(pipes[j][0]);
+        close(pipes[j][1]);
+    }
+
+    if (!command->background) {
+        for (int j = 0; j <= num_pipes; j++) {
+            wait(0); 
+        }
+    }
+
+	return SUCCESS;
+}
